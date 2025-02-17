28220f309a86d69dc159d53c108c9c7730aeb8aa
xRubin <dalamar84@mail.ru>
2025-02-17 21:11:10 +0300

    #FT-4900

--- src/services/trip/extractors/TripFlightTtlExtractor.php
+++ src/services/trip/extractors/TripFlightTtlExtractor.php
@@ -9,6 +9,9 @@ final class TripFlightTtlExtractor implements TripExtractorInterface
     public function extractValue(TripDecomposition $trip): int
     {
         $flights = $trip->getTaxiFlights();
+        // #FT-4900
+        if (count($flights) == 0)
+            return 9999;
         $firstFlight = reset($flights);
         $timeLeft = $firstFlight->getDepartureDate()->getTimestamp() - time();
         return $timeLeft <= 0 ? 0 : intval($timeLeft / 3600);
